693e409510e063248e77b8f37bca5ea3147c9803
joaoojohn <joaopedro.cs12@hotmail.com>
2021-07-20 14:32:10 -0300

    Utilizando Identity e Entity Framework

--- ByteBank.Forum/Controllers/ContaController.cs
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -1,4 +1,6 @@
-﻿using ByteBank.Forum.ViewModels;
+﻿using ByteBank.Forum.Models;
+using ByteBank.Forum.ViewModels;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,8 +18,22 @@ namespace ByteBank.Forum.Controllers
 
         [HttpPost]
         public ActionResult Registrar(ContaRegistrarViewModel model)
-        {
-            return View();
+        { 
+            if (ModelState.IsValid)
+            {
+                var dbContext = new IdentityDbContext<UsuarioAplicacao>();
+                UsuarioAplicacao identityUser = new UsuarioAplicacao();
+
+                identityUser.Email = model.Email;
+                identityUser.UserName = model.UserName;
+                identityUser.NomeCompleto = model.NomeCompleto;
+
+                dbContext.Users.Add(identityUser);
+                dbContext.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
         }
     }
 }
